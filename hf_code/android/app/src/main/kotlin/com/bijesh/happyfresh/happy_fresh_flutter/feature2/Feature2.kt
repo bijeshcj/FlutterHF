786d7786cf39e33942aed0426936e296d25c48ad
bijeshjay <bijesh.jayaprakash@gmail.com>
2022-04-30 14:45:05 +0530

    adding feature 2 new feature

--- hf_code/android/app/src/main/kotlin/com/bijesh/happyfresh/happy_fresh_flutter/feature2/Feature2.kt
+++ hf_code/android/app/src/main/kotlin/com/bijesh/happyfresh/happy_fresh_flutter/feature2/Feature2.kt
@@ -3,7 +3,7 @@ package com.bijesh.happyfresh.happy_fresh_flutter.feature2
 class Feature2 {
 
     fun feature2NewFeature30Apr1435(){
-
+        print("Main has made this change on 30 Apr at 14:44")
     }
 
     fun about() : String{
@@ -11,7 +11,7 @@ class Feature2 {
     }
 
     fun trackEvent(){
-
+        print("Main has made this change on 30 Apr at 14:44")
     }
 
 }
