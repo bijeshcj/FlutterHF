46875915687246d239552350fece3e2406a737c9
bijeshjay <bijesh.jayaprakash@gmail.com>
2022-04-30 14:46:44 +0530

    adding print in feature2NewFeature30Apr1435 and trackEvent

--- hf_code/android/app/src/main/kotlin/com/bijesh/happyfresh/happy_fresh_flutter/feature2/Feature2.kt
+++ hf_code/android/app/src/main/kotlin/com/bijesh/happyfresh/happy_fresh_flutter/feature2/Feature2.kt
@@ -3,7 +3,7 @@ package com.bijesh.happyfresh.happy_fresh_flutter.feature2
 class Feature2 {
 
     fun feature2NewFeature30Apr1435(){
-
+        println("Feature1 branch has made this change on 30 apr 14:45")
     }
 
     fun about() : String{
@@ -11,7 +11,7 @@ class Feature2 {
     }
 
     fun trackEvent(){
-
+        println("Feature1 branch has made this change on 30 apr 14:45")
     }
 
 }
