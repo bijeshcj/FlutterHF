0740c046709f23997bdb8a1e1436678915baaa49
bijeshjay <bijesh.jayaprakash@gmail.com>
2022-05-09 15:21:39 +0530

    release 73 - feature search bijesh

---
+++ hf_code/android/app/src/main/kotlin/com/bijesh/happyfresh/happy_fresh_flutter/release73/feature_bijesh/Feature_SR15.kt
@@ -0,0 +1,9 @@
+package com.bijesh.happyfresh.happy_fresh_flutter.release73.feature_bijesh
+
+class Feature_SR15 {
+
+    fun feature_sr15(){
+        println("am writing the code for sr 15")
+    }
+
+}
